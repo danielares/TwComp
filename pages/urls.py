e187e82db12d7d569dd0a6e7530ce90272d5ce84
Daniel Ares Moraes <daniel.ares@hotmail.com>
2022-03-16 10:29:15 -0300

    graficos chartjs primeira versão

--- pages/urls.py
+++ pages/urls.py
@@ -4,7 +4,6 @@ from django.urls import path, include
 from usuarios.views import IndexView, CadastroView
 from tweets.views import TakeTweetsView, ViewTweetsView
 from compareTweets.views import ViewCompareTweetsView, CompareTweetsView
-from .views import chartsView
 
 urlpatterns = [
     path('contas/', include('django.contrib.auth.urls')),
@@ -14,5 +13,4 @@ urlpatterns = [
     path('viewtweets/', ViewTweetsView.as_view(), name='viewtweets'),
     path('compare-tweets/', ViewCompareTweetsView.as_view(), name='compareTweets'),
     path('view-compare-tweets/', CompareTweetsView.as_view(), name='view-compare-tweets'),
-    path('charts/', chartsView.as_view(), name='charts'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,2 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-
-
-class chartsView(TemplateView):
-    template_name = 'charts/chart2.html'
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        return context
