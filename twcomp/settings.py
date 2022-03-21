b84dbd16d07814813448935f86726299d8ea75ff
Daniel Ares Moraes <daniel.ares@hotmail.com>
2022-03-21 07:31:07 -0300

    deploy

--- twcomp/settings.py
+++ twcomp/settings.py
@@ -133,8 +133,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
