d09e03dcdaa4a220f2d7d53396cbe4cdb41787d9
Daniel Ares Moraes <daniel.ares@hotmail.com>
2022-03-21 19:33:26 -0300

    fixed word cloud errors

--- compareTweets/views.py
+++ compareTweets/views.py
@@ -72,10 +72,12 @@ class CompareTweetsView(TemplateView):
                            "chartsInfo1": chartsInfo1, "chartsInfo2": chartsInfo2,
                            "tweets1": tweets1, "tweets2": tweets2}
                     
-                    wordCloud(tweets1, search1)
-                    wordCloud(tweets2, search2)
+                    wordcloud1 = wordCloud(tweets1, search1)
+                    wordcloud2 = wordCloud(tweets2, search2)
 
                     context = super().get_context_data(**kwargs)
+                    context['wordcloud1'] = wordcloud1
+                    context['wordcloud2'] = wordcloud2
                     context['option'] = option
                     context['amoutTweets'] = amoutTweets
                     context['term1'] = search1
