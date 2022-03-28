24faa751653260f1d4b81c79e64081e83f5716df
Daniel Ares Moraes <daniel.ares@hotmail.com>
2022-03-28 20:33:43 -0300

    ordem alfabetica dicionarios

--- myLibs/return_data_view.py
+++ myLibs/return_data_view.py
@@ -14,7 +14,7 @@ def get_tweets(search, amoutTweets, option, bearerToken):
 def generate_data(tweets, option):
     
     if option == 'simple': colors = ['red','gray','green']
-    elif option == 'advanced': colors = ['gray', 'violet', 'green',  'red', 'blue', 'yellow']
+    elif option == 'advanced': colors = ['yellow', 'violet', 'green',  'red', 'blue', 'gray']
     
     # Convert a List of dictionaries using from_records() method.
     df = pd.DataFrame(tweets)
@@ -23,7 +23,10 @@ def generate_data(tweets, option):
     for sentiment in df['tweet_analise']: sentiments.append(sentiment[0])
     df['sentiment'] = sentiments
     
+    # Create a dictionary with the number of values per sentiment
     count_sentiments = df['sentiment'].value_counts().to_dict()
+    # Alphabetical order of dictionary keys. Required for elements not to be in the wrong position in Compare Tweets
+    count_sentiments = {key: value for key, value in sorted(count_sentiments.items())}
     
     # List with dictionary keys
     labels = [label for label in count_sentiments.keys()]
