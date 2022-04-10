7fbfbcbb1de319413dddccb16fdfa36af7027625
Daniel Ares Moraes <daniel.ares@hotmail.com>
2022-04-10 16:55:00 -0300

    modify user

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 
-from usuarios.views import IndexView, CreateUserView
+from usuarios.views import IndexView, CreateUserView, ModifyUserView
 
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('', IndexView.as_view(), name='index'),
     path('create-user/', CreateUserView.as_view(), name='create_user'),
+    path('modify-user/', ModifyUserView.as_view(), name='modify-user'),
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,46 +1,64 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, View
 from django.urls import reverse_lazy
 from django.contrib.auth.models import auth
+from django.contrib import messages
 
 from .models import CustomUsuario
 
 
 class CreateUserView(TemplateView):
     template_name = 'registration/cadastro.html'
-    #form_class = CustomUsuarioCreateForm
-    success_url = reverse_lazy('login')
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
 
     def post(self, request):
-        if request.method == 'POST':
-            first_name = request.POST['firstname']
-            last_name = request.POST['lastname']
-            email = request.POST['email']
-            password1 = request.POST['password1']
-            
-            consumerKey = request.POST['consumerKey']
-            consumerSecret = request.POST['consumerSecret']
-            accessToken = request.POST['accessToken']
-            accessTokenSecret = request.POST['accessTokenSecret']
-            bearerToken = request.POST['bearerToken']
-            
-            usuario = CustomUsuario.objects.create_user(first_name=first_name, 
-                                                        last_name=last_name, 
-                                                        email=email, 
-                                                        password=password1, 
-                                                        consumerKey=consumerKey, 
-                                                        consumerSecret=consumerSecret, 
-                                                        accessToken=accessToken, 
-                                                        accessTokenSecret=accessTokenSecret, 
-                                                        bearerToken=bearerToken)
-            return render(request, 'index.html')
+        first_name = request.POST['firstname']
+        last_name = request.POST['lastname']
+        email = request.POST['email']
+        password1 = request.POST['password1']
         
-        else:
-            return render(request, 'index.html')
+        consumerKey = request.POST['consumerKey']
+        consumerSecret = request.POST['consumerSecret']
+        accessToken = request.POST['accessToken']
+        accessTokenSecret = request.POST['accessTokenSecret']
+        bearerToken = request.POST['bearerToken']
+        
+        usuario = CustomUsuario.objects.create_user(first_name=first_name, 
+                                                    last_name=last_name, 
+                                                    email=email, 
+                                                    password=password1, 
+                                                    consumerKey=consumerKey, 
+                                                    consumerSecret=consumerSecret, 
+                                                    accessToken=accessToken, 
+                                                    accessTokenSecret=accessTokenSecret, 
+                                                    bearerToken=bearerToken)
+        return render(request, 'registration/login.html')
+    
+
+class ModifyUserView(TemplateView):
+    template_name = 'registration/modificar.html'
+
+    def post(self, request):
+        usuario = request.user
+        
+        usuario.first_name = request.POST['firstname']
+        usuario.last_name = request.POST['lastname']
+        usuario.email = request.POST['email']
+        
+        usuario.consumerKey = request.POST['consumerKey']
+        usuario.consumerSecret = request.POST['consumerSecret']
+        usuario.accessToken = request.POST['accessToken']
+        usuario.accessTokenSecret = request.POST['accessTokenSecret']
+        usuario.bearerToken = request.POST['bearerToken']
+        usuario.save()
+
+        messages.success(request, 'Seu perfil foi atualizado com sucesso!')
+        
+        return render(request, self.template_name)
+        
           
  
 class IndexView(TemplateView):
