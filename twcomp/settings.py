023f55737e9ef52d4ce2685e9af4c71ca9ca96e5
Daniel Ares Moraes <daniel.ares@hotmail.com>
2022-05-02 10:14:40 -0300

    deploy

--- twcomp/settings.py
+++ twcomp/settings.py
@@ -15,8 +15,6 @@ import dj_database_url
 from pathlib import Path
 import os
 
-from django_key import SECRET_KEY_DJANGO
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
--- twcomp/settings_local.py
+++ twcomp/settings_local.py
@@ -14,9 +14,6 @@ import os
 import dj_database_url
 from pathlib import Path
 
-from django_key import SECRET_KEY_DJANGO
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY_DJANGO
+SECRET_KEY = 'django-insecure-2wd6r-evddy0vgt1=sw9714tv0b_7mike#p4hqg6#w83f7u+n$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
